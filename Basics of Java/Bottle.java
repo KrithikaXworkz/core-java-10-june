f1e3fa8928be84143493196c661b90a3b708235f
Krithika <krithika.xworkz@gmail.com>
2024-07-04 11:00:01 +0530

    java folder

--- Basics of Java/Bottle.java
+++
@@ -1,9 +0,0 @@
-class Bottle{
-    
-	public static void main(String[] solaraInsulated){
-	
-	System.out.println(" Bottle is a narrow-necked container made of an impermeable material (such as glass, plastic or aluminium) in various shapes and sizes that stores and transports liquids.");
-	System.out.println("Its mouth, at the bottling line, can be sealed with an internal stopper, an external bottle cap, a closure, or induction sealing.");
-	System.out.println("narrow-necked, rigid or semirigid container that is primarily used to hold liquids and semiliquids.");
-	}
-}
