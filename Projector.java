032c959b0100e0e5b28c1c4d33ab9172b92db392
Krithika <krithika.xworkz@gmail.com>
2024-07-05 22:08:24 +0530

    Commiting the Methods related Java Files

---
+++ Projector.java
@@ -0,0 +1,11 @@
+class Projector{
+
+    public static void display(){
+	
+	System.out.println("display method added");
+	System.out.println("Projector works that allows us to display images, videos, or other content on a large screen or surface.");
+	System.out.println("display method ended");
+	return;
+	}
+	
+}
