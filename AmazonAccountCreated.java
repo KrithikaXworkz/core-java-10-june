2f3cc63b22569e3ba8ab3bcf4b40df3ae2f4d0fa
Krithika <krithika.xworkz@gmail.com>
2024-07-23 21:04:02 +0530

    Commiting CRUD Operations related java files 23/07/2024

--- AmazonAccountCreated.java
+++ AmazonAccountCreated.java
@@ -10,7 +10,7 @@ class AmazonAccountCreated{
     public static boolean createUserAccount(String firstName, String lastName, long mobileNumber, String email, String pwd, String confirmPwd){
 	
 	     boolean isAmazonAccountCreated = false ;
-		 boolean isFirstNameValid = false ;
+		 /*boolean isFirstNameValid = false ;
 		 boolean isLastNameValid = false ;
 		 boolean isMobileNumberValid = false ;
 		 boolean isEmailValid = false ;
@@ -60,12 +60,72 @@ class AmazonAccountCreated{
 			System.out.println("Confirm Password Invalid");
 		 
         if(isFirstNameValid == true && isLastNameValid == true && isMobileNumberValid == true && isEmailValid == true && isPwdValid == true && isConfirmPwdValid == true) 
-		isAmazonAccountCreated = true ;
+		isAmazonAccountCreated = true ;*/
+	
+	    isAmazonAccountCreated = validatingAmazonAccountUserDetails(firstName, lastName, mobileNumber, email, pwd, confirmPwd);
 		 
 		 return isAmazonAccountCreated ;
 	
 	}
     
+	public static boolean validatingAmazonAccountUserDetails(String firstName, String lastName, long mobileNumber, String email, String pwd, String confirmPwd){
+		boolean isAllFieldValidated = false ;
+		boolean isFirstNameValid = false ;
+		 boolean isLastNameValid = false ;
+		 boolean isMobileNumberValid = false ;
+		 boolean isEmailValid = false ;
+		 boolean isPwdValid = false ;
+		 boolean isConfirmPwdValid = false ;
+		 
+		 if(firstName != null){
+		    givenName = firstName ;
+            isFirstNameValid = true ;			
+		 }
+		 else
+			 System.out.println("First Name is Invalid");
+		 
+		 if(lastName != null){
+			surName = lastName;
+			isLastNameValid = true ; 
+		 }
+		 else
+			System.out.println("Last Name is Invalid");
+		 
+		if(mobileNumber != 0.0){
+			 phoneNumber = mobileNumber ;
+			 isMobileNumberValid = true ;
+		}
+		else
+			System.out.println("Mobile Number is Invaid");
+		
+		if(email != null){
+			 mailId = email ;
+			 isEmailValid = true ;
+		}
+		else
+			System.out.println("Email Invalid");
+		
+		if(pwd != null){
+			password = pwd ;
+			isPwdValid = true ;
+		}
+		else
+			System.out.println("Password Invalid");
+		
+		if(confirmPwd != null){
+			confirmPassword = confirmPwd ;
+			isConfirmPwdValid = true ;
+		}
+		else
+			System.out.println("Confirm Password Invalid");
+		 
+        if(isFirstNameValid == true && isLastNameValid == true && isMobileNumberValid == true && isEmailValid == true && isPwdValid == true && isConfirmPwdValid == true) 
+		isAllFieldValidated = true ;
+	
+	return isAllFieldValidated ;
+		
+		
+	}
 	public static void readUserAccountDetails(){
 	
 	    System.out.println("fetching... Student Details...");
--- SpotifyAccountCreated.java
+++ SpotifyAccountCreated.java
@@ -9,6 +9,61 @@ class SpotifyAccountCreated{
     public static boolean createSpotifyAccount(String userName, String pwd, String email, String confirmEmail, String dob){
 	
 	    boolean isSpotifyAccountCreated = false ;
+		/*boolean isUserNameValid = false ;
+		boolean isPwdValid = false ;
+		boolean isEmailValid = false ;
+		boolean isConfirmEmailValid = false ;
+		boolean isDobValid = false ;
+		
+		//validation  //  null check 
+		    if(userName != null){
+			    givenName = userName ;
+				isUserNameValid = true ;
+			}
+			else
+			    System.out.println("User Name Invalid");
+				
+			if(pwd != null){
+			    password = pwd ;
+				isPwdValid = true ;
+			}
+			else 
+			    System.out.println("Password Invalid");
+				
+			if(email != null){
+			    emailId = email ;
+				isEmailValid = true ;
+			}
+			else 
+			    System.out.println("Email Id Invalid");
+				
+			if(confirmEmail != null){
+			    confirmEmailId = confirmEmail ;
+				isConfirmEmailValid = true ;
+			}
+			else
+			    System.out.println("Confirm Email Id Invalid");
+				
+			if(dob != null){
+			    dateOfBirth = dob ;
+				isDobValid = true ;
+			}
+			else
+			    System.out.println("Date of Birth is Invalid");
+		    
+			
+			if(isUserNameValid == true && isPwdValid == true && isEmailValid == true && isConfirmEmailValid == true && isDobValid == true)
+			isSpotifyAccountCreated = true ;*/
+		
+		isSpotifyAccountCreated = validatingSpotifyAccountUserDetails(userName, pwd, email, confirmEmail, dob);
+		
+		
+		
+		return isSpotifyAccountCreated;
+	}
+    
+	public static boolean validatingSpotifyAccountUserDetails(String userName, String pwd, String email, String confirmEmail, String dob){
+		boolean isAllFieldValidated = false ;
 		boolean isUserNameValid = false ;
 		boolean isPwdValid = false ;
 		boolean isEmailValid = false ;
@@ -53,16 +108,10 @@ class SpotifyAccountCreated{
 		    
 			
 			if(isUserNameValid == true && isPwdValid == true && isEmailValid == true && isConfirmEmailValid == true && isDobValid == true)
-			isSpotifyAccountCreated = true ;
+			isAllFieldValidated = true ;
+		return isAllFieldValidated ;
 		
-		
-		
-		
-		
-		
-		return isSpotifyAccountCreated;
 	}
-    
 	public static void readSpotifyAccountDetails(){
 		
 		 
